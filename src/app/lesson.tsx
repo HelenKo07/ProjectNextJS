9cbaad3b7235e4d2b2d76b13b82dd4acc69c3e22
Alena_Korma <alena.gorova93@gmail.com>
2026-03-11 17:05:31 +0200

    fix styles

--- src/app/lesson.tsx
+++ src/app/lesson.tsx
@@ -13,9 +13,7 @@ type LikeState = "unset" | "Liked" | "Dislike";
 
 export function Lessons({ name, subtitle, points }: LessonProps) {
     const [likeState, setLikeState] = useState<LikeState>("unset");
-  //   const [disState, setDisState] = useState<LikeState>("unset");
     const setLike = () => setLikeState("Liked");
-  //   const setDislike = () => setDisState("Dislike");
 
   return (
     <details>
@@ -27,9 +25,6 @@ export function Lessons({ name, subtitle, points }: LessonProps) {
         <button className={styles.buttonLike} type="button" onClick={setLike}>
           {likeState === "unset" ? "like" : likeState}
         </button>{" "}
-        {/* <button className={styles.buttonDis} type="button" onClick={setDislike}>
-          {disState === "unset" ? "dislike" : disState}
-        </button> */}
       </p>
       <ol className={styles.points}>
         {points.map((point, index) => {
